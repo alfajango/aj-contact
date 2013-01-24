5acf38ecfd768fba06d3c50b6787a7ad55278368
Steve Schwartz <steve@alfajango.com>
2013-01-24 10:59:00 -0500

    Added email functionality to notify of contact submission.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "bundler/setup"
 
 require 'sinatra'
 require 'sequel'
+require 'pony'
 
 configure do
   DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://postgres@localhost/aj_contact')
@@ -20,6 +21,7 @@ class Submission < Sequel::Model; end
 post '/contact' do
   puts "Contact submission:"
   puts params
-  Submission.create(:name => params[:name], :email => params[:email], :message => params[:message])
+  Submission.create(:name => params[:full_name], :email => params[:email], :message => params[:message])
+  Pony.mail :to => "support@alfajango.com", :from => params[:email], :subject => "[AJ Contact Form] Submission from #{params[:full_name]}", :body => erb(:email)
   redirect 'http://localhost:4000/thank_you'
 end
